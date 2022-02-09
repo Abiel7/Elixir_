de0e063cc764bc37b064b71200a0d5e7f617366f
abk <abiekidane19@hotmail.com>
2022-02-09 11:31:22 +0100

    List Comprehension

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,16 +1,50 @@
 defmodule Cards do
-
   def create_dec do
-    ["Ace","Two","three"]
+    values = [
+      "Ace",
+      "Two",
+      "three",
+      "four",
+      "five",
+      "six",
+      "seven",
+      "eight",
+      "nine",
+      "ten",
+      "Jack",
+      "Queen",
+      "King"
+    ]
+
+    suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+
+    # List comprehension,  for each  element  in suits do somthing
+    #  this returns  new
+
+    # option  one
+    # cards =
+    #   for values <- values do
+    #     for suits <- suits do
+    #       "#{values} of #{suits}"
+    #     end
+    #   end
+
+    #   List.flatten(cards)
+
+    # option  two
+    for suit <-suits,value <- values do
+        "#{value} of #{suit}"
+    end
   end
 
-  def shuffle(deck ) do
-      Enum.shuffle(deck)
+  def shuffle(deck) do
+    Enum.shuffle(deck)
   end
 
-  def contains?(deck,card) do
-    Enum.member?(deck,card)
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
   end
+
 
 end
 
