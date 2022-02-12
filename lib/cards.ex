7c5b2423e7bde1c888b24d5a7a313b316436f422
abk <abiekidane19@hotmail.com>
2022-02-12 22:23:30 +0100

    pattern matching

--- lib/cards.ex
+++ lib/cards.ex
@@ -47,12 +47,32 @@ defmodule Cards do
   end
 
   def deal(deck, hand_siz) do
-    Enum.split(deck, hand_siz) 
+    Enum.split(deck, hand_siz)
 
     # returns a tuple  with two  elements
   end
 
+  def save(deck, fileName) do
+    binary = :erlang.term_to_binary(deck)
 
+    File.write(fileName, binary)
+    # File.write("#{fileName}.txt", binary)
+  end
+
+  def load(fileName) do
+    # option  1
+    # {status, content} = File.read(fileName)
+    # case status do
+    #   :ok -> :erlang.binary_to_term(content)
+    #   :error -> "The file  #{fileName}  could not be read"
+    # end
+
+    # option  2
+    case File.read!(fileName) do
+      {:ok,binary } -> :erlang.binary_to_term binary
+      {:error, _message } -> "The file  #{fileName}  could not be read"
+    end
+  end
 end
 
 # start interactive elixir shell by  iex -S mix
@@ -61,3 +81,11 @@ end
 # shuffled = Cards.shuffle(dec)
 
 # IO.puts(shuffled |> Enum.join(" "))
+
+## pattern matching
+# two  is pont to hands and the remaining list  to  rest
+# Enum.split(dec, 2) returns  a tuple with  two  elemetns,  the list  and  count
+# with  count being at index 0 and list at index 1
+{hands, rest} = Cards.deal(Cards.create_dec() |> Cards.shuffle(), 2)
+fileName = "myfile"
+# every time  you see  = think about  the  pattern  matching
