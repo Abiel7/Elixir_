e87d14ae6b481e1f0715a1c4f46cc5fac413b8c1
abk <abiekidane19@hotmail.com>
2022-02-13 13:58:07 +0100

    added  som test cases

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -1,5 +1,15 @@
 defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
-  
+
+  test "create deck  of cards" do
+    deck_length = length(Cards.create_dec())
+    assert deck_length == 52
+  end
+
+  test "shuffling the deck  and randomize it" do
+    deck_shffuled = Cards.shuffle(Cards.create_dec())
+    # assert deck_shffuled != Cards.create_dec()
+    refute deck_shffuled == Cards.create_dec()
+  end
 end
