ead86bcabdf8c981935be422875a297e23e2b3f6
abiel <abielkidane19@hotmai.com>
2022-02-22 15:16:21 +0100

    ident fnctions added

--- ident/lib/ident.ex
+++ ident/lib/ident.ex
@@ -3,14 +3,34 @@ defmodule Ident do
     input
     |> hash_input
     |> pick_color
+    |> build_grid
   end
 
-  def pick_color(image) do
-    %Ident.Image{hex: [r, g, b | _tail]} = image
+  @spec build_grid(any) :: list
+  def build_grid(%Ident.Image{hex: hex} = image) do
+    hex
+    |> Enum.chunk_every(3)
+    
+  end
 
-  # Do not modify  existing data  always create a new record 
+  def miror_rows(rows) do
+    [first,second | _tail] = rows
+    rows ++ [second,first]
+  end
+
+  def pick_color(%Ident.Image{hex: [r, g, b | _tail]} = image) do
     %Ident.Image{image | color: {r, g, b}}
   end
+
+  # how  the above  function will look like writen in JS
+  # let pick_colour = function(image) {
+  #     image.color = {
+  #         r: image.hex[0],
+  #           g: image.hex[1],
+  #           b: image.hex[2] ;
+  #       }
+  #       return image;
+  # }
 
   @spec hash_input(any) :: %Ident.Image{hex: [byte]}
   def hash_input(input) do
