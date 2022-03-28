0591f281c652e43d0ea5b8449624485fe798258b
abiel <abielkidane19@hotmai.com>
2022-03-28 12:44:57 +0200

    added som methods for ident

--- ident/lib/ident.ex
+++ ident/lib/ident.ex
@@ -6,19 +6,50 @@ defmodule Ident do
     |> build_grid
     |> filter_odd
     |> build_image
+    |> draw_image
+    |> save_image
   end
 
+  
+
+  # draw image using erlang graphical drawer
+  def draw_image(%Ident.Image{color: color, pixel_map: pixel_map}) do
+      image = :egd.create(250,250)
+      fill = :egd.color(color)
+
+      Enum.each(pixel_map, fn({start,stop})->
+          :edg.filedRectangel(image, start, stop, fill)
+      end )
+
+      :edg.render(image)
+
+  end
+
+  # build image
+  # rem(index,50) * 50 ->
+  # div(index,50) * 50  \>
   def build_image(%Ident.Image{grid: grid} = image) do
-       
+    pixel_map =
+      Enum.map(grid, fn {_x, index} ->
+        horizontal = rem(index, 5) * 50
+        vertical = div(index, 5) * 50
+
+        top_left = {horizontal, vertical}
+        bottom_right = {horizontal + 50, vertical + 50}
+
+        {top_left, bottom_right}
+      end)
+
+    %Ident.Image{image | pixel_map: pixel_map}
   end
 
   def filter_odd(%Ident.Image{grid: grid} = image) do
-    grid = Enum.filter grid, fn ({code,_index}) ->
-      rem(code,2) == 0
-    end
+    grid =
+      Enum.filter(grid, fn {code, _index} ->
+        rem(code, 2) == 0
+      end)
 
-    %Ident.Image{image | grid: grid }
-
+    %Ident.Image{image | grid: grid}
   end
 
   @spec build_grid(any) :: list
